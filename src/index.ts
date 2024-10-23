60c0a7a9ec9838ba0025a62401e70e3f8341a359
yoon.homme <woosiks.io@gmail.com>
2024-10-23 14:14:28 +0900

    feat: 클라이언트에서 채팅 입력시 메시지를 전송하게 함

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,8 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
 
