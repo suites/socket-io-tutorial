7513659075d7f79f76541091fbfcd8e2fcb6b6eb
yoon.homme <woosiks.io@gmail.com>
2024-10-23 15:37:08 +0900

    feat: 연결이 끊어졌을 때 대화내용 복원하는 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,62 @@ import express, { Request, Response } from 'express';
 import { join } from 'path';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {},
-});
-
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(join(__dirname, 'static/index.html'));
-});
-
-io.on('connection', (socket) => {
-  io.emit('chat message', '상대방과 연결되었습니다.');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+async function main() {
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database,
   });
-});
 
-server.listen(30000, () => {
-  console.log(`Server is running at http://localhost:30000`);
-});
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+  });
+
+  app.get('/', (req: Request, res: Response) => {
+    res.sendFile(join(__dirname, 'static/index.html'));
+  });
+
+  io.on('connection', async (socket) => {
+    io.emit('chat message', '상대방과 연결되었습니다.');
+    socket.on('chat message', async (msg) => {
+      let result: any;
+      try {
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        return;
+      }
+
+      io.emit('chat message', msg, result.lastId);
+    });
+
+    if (!socket.recovered) {
+      try {
+        await db.each(
+          'SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          },
+        );
+      } catch (e) {}
+    }
+  });
+
+  server.listen(30000, () => {
+    console.log(`Server is running at http://localhost:30000`);
+  });
+}
+
+main();
