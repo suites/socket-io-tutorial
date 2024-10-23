17783074989db49549e3313c5bd9fa7b797945bc
yoon.homme <woosiks.io@gmail.com>
2024-10-23 13:54:38 +0900

    feat: socket.io 연결

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from 'express';
 import { join } from 'path';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const app = express();
-const port = 3000;
+const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(join(__dirname, 'static/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+io.on('connection', (socket) => {
+  console.log(socket);
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(30000, () => {
+  console.log(`Server is running at http://localhost:30000`);
 });
